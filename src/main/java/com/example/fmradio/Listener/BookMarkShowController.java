4a4485893552ec5acca15dad09288a21d223d8f8
awesomeahnaf <ahnaf7662@gmail.com>
2025-08-05 15:29:33 +0600

    a new fxml and controller

---
+++ src/main/java/com/example/fmradio/Listener/BookMarkShowController.java
@@ -0,0 +1,31 @@
+package com.example.fmradio.Listener;
+
+public class BookMarkShowController
+{
+    @javafx.fxml.FXML
+    private ComboBox showGenreTextField;
+    @javafx.fxml.FXML
+    private TableColumn showGenreColumn;
+    @javafx.fxml.FXML
+    private ComboBox bookMarkCategoryComboBox;
+    @javafx.fxml.FXML
+    private TableColumn showTitleColumn;
+    @javafx.fxml.FXML
+    private TableColumn bookMarkCategoryColumn;
+    @javafx.fxml.FXML
+    private ComboBox showTitleTextField;
+    @javafx.fxml.FXML
+    private TableView bookMarkTableView;
+    @javafx.fxml.FXML
+    private TableColumn dateOfBookMarkColumn;
+    @javafx.fxml.FXML
+    private DatePicker bookMarkDateDatePicker;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveBookMarkButtonOnAction(ActionEvent actionEvent) {
+    }
+}
