d6742a30e1c4c4136302b861103b64125f05b754
awesomeahnaf <ahnaf7662@gmail.com>
2025-08-08 16:25:25 +0600

    Listner's one goal done

--- src/main/java/com/example/fmradio/Listener/ListenerPollParticipationController.java
+++ src/main/java/com/example/fmradio/Listener/ListenerPollParticipationController.java
@@ -1,5 +1,12 @@
 package com.example.fmradio.Listener;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ListenerPollParticipationController
 {
     @javafx.fxml.FXML
--- src/main/java/com/example/fmradio/Listener/ReportShowController.java
+++ src/main/java/com/example/fmradio/Listener/ReportShowController.java
@@ -1,5 +1,8 @@
 package com.example.fmradio.Listener;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class ReportShowController
 {
     @javafx.fxml.FXML
